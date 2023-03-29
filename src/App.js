a03f19d84d662ba31a4216a35463eb556edf0b3d
rfabreu <raabreugomes@gmail.com>
2023-03-29 16:31:22 -0400

    Built initial landing page structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h4 className='header'>CARLA ANTUNES / Freckles Design</h4>
+      <video className='video' src='./coming_soon.mp4' autoplay loop controls /> {/* add controls={false} to hide video controls */}
+      <div className='content'>
+        <h1 className='main-heading'>Will be celebrating the<br />
+        lauch of our new site soon.</h1>
+        <div className='buttons'>
+          <button className='btn btn-outline-light' onClick={() => window.open('https://linktr.ee/carla_antunes?fbclid=PAAaZvEwpQjeWTT9XHSHy9EZIPtEvInpc4yJlwmpryAdYnOOZ7vpgdlpG2MAA', '_blank', 'noopener noreferrer')}>Follow me</button>
+          <button className='btn btn-outline-light' onClick={() => window.open('https://instagram.com/_freckles_design?igshid=YmMyMTA2M2Y=', '_blank', 'noopener noreferrer')}>
+            <FontAwesomeIcon icon={faArrowRight} /> @_freckles_design
+          </button>
+          <button className='btn btn-outline-light' onClick={() => window.location.href='mailto:carla@frecklesdesign.ca'}>
+            <FontAwesomeIcon icon={faArrowRight} /> carla@frecklesdesign.ca
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
