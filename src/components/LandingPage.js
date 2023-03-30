0340cba3e829397fdc6758eae8a7485be7c70c0e
rfabreu <raabreugomes@gmail.com>
2023-03-30 16:45:30 -0400

    Adjusted button elements

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -13,12 +13,12 @@ function LandingPage() {
                 <h1 className='main-heading'>Will be celebrating the<br />
                     lauch of our new site soon.</h1>
                 <div className='buttons'>
-                    <button className='btn btn-outline-light btn-lg btn-block' onClick={() => window.open('https://linktr.ee/carla_antunes?fbclid=PAAaZvEwpQjeWTT9XHSHy9EZIPtEvInpc4yJlwmpryAdYnOOZ7vpgdlpG2MAA', '_blank', 'noopener noreferrer')}>Follow me</button>
-                    <button className='btn btn-outline-light btn-lg btn-block' onClick={() => window.open('https://instagram.com/_freckles_design?igshid=YmMyMTA2M2Y=', '_blank', 'noopener noreferrer')}>
-                        <FontAwesomeIcon icon={faArrowRight} /> @_freckles_design
+                    <button className='btn btn-outline-light btn-lg' onClick={() => window.open('https://linktr.ee/carla_antunes?fbclid=PAAaZvEwpQjeWTT9XHSHy9EZIPtEvInpc4yJlwmpryAdYnOOZ7vpgdlpG2MAA', '_blank', 'noopener noreferrer')}>Follow me</button>
+                    <button className='btn btn-outline-light btn-lg' onClick={() => window.open('https://instagram.com/_freckles_design?igshid=YmMyMTA2M2Y=', '_blank', 'noopener noreferrer')}>
+                        <FontAwesomeIcon icon={faArrowRight} className='btn-icon' /> @_freckles_design
                     </button>
-                    <button className='btn btn-outline-light btn-lg btn-block' onClick={() => window.location.href = 'mailto:carla@frecklesdesign.ca'}>
-                        <FontAwesomeIcon icon={faArrowRight} /> carla@frecklesdesign.ca
+                    <button className='btn btn-outline-light btn-lg' onClick={() => window.location.href = 'mailto:carla@frecklesdesign.ca'}>
+                        <FontAwesomeIcon icon={faArrowRight} className='btn-icon' /> carla@frecklesdesign.ca
                     </button>
                 </div>
             </div>
