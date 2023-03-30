ccdc1948e33fc51b55e6c90272a292b7ebcb41c1
rfabreu <raabreugomes@gmail.com>
2023-03-30 15:37:32 -0400

    Added video miniature animation

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import News from "../coming_soon.mp4";
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
@@ -7,16 +8,16 @@ function LandingPage() {
     return (
         <div className='App'>
             <h4 className='header'>CARLA ANTUNES / Freckles Design</h4>
-            <video className='video' src='./coming_soon.mp4' autoplay loop controls /> {/* add controls={false} to hide video controls */}
+            <video className='video' src={News} autoPlay loop muted controls={false} />
             <div className='content'>
                 <h1 className='main-heading'>Will be celebrating the<br />
                     lauch of our new site soon.</h1>
                 <div className='buttons'>
-                    <button className='btn btn-outline-light' onClick={() => window.open('https://linktr.ee/carla_antunes?fbclid=PAAaZvEwpQjeWTT9XHSHy9EZIPtEvInpc4yJlwmpryAdYnOOZ7vpgdlpG2MAA', '_blank', 'noopener noreferrer')}>Follow me</button>
-                    <button className='btn btn-outline-light' onClick={() => window.open('https://instagram.com/_freckles_design?igshid=YmMyMTA2M2Y=', '_blank', 'noopener noreferrer')}>
+                    <button className='btn btn-outline-light btn-lg btn-block' onClick={() => window.open('https://linktr.ee/carla_antunes?fbclid=PAAaZvEwpQjeWTT9XHSHy9EZIPtEvInpc4yJlwmpryAdYnOOZ7vpgdlpG2MAA', '_blank', 'noopener noreferrer')}>Follow me</button>
+                    <button className='btn btn-outline-light btn-lg btn-block' onClick={() => window.open('https://instagram.com/_freckles_design?igshid=YmMyMTA2M2Y=', '_blank', 'noopener noreferrer')}>
                         <FontAwesomeIcon icon={faArrowRight} /> @_freckles_design
                     </button>
-                    <button className='btn btn-outline-light' onClick={() => window.location.href = 'mailto:carla@frecklesdesign.ca'}>
+                    <button className='btn btn-outline-light btn-lg btn-block' onClick={() => window.location.href = 'mailto:carla@frecklesdesign.ca'}>
                         <FontAwesomeIcon icon={faArrowRight} /> carla@frecklesdesign.ca
                     </button>
                 </div>
