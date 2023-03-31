32d94f90179f98836c7de7186429271ecccc8a14
rfabreu <raabreugomes@gmail.com>
2023-03-31 16:30:21 -0400

    Created Footer component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import LandingPage from './components/LandingPage';
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
   return (
     <main>
       <LandingPage />
+      <Footer />
     </main>
   );
 }
