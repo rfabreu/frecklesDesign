dc64bf41d6d652dbff81ddcfcf9dd85d2ae64dd8
rfabreu <raabreugomes@gmail.com>
2023-04-02 21:40:17 -0400

    Fixed grammar

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -10,8 +10,8 @@ function LandingPage() {
             <h4 className='header'><strong>CARLA ANTUNES</strong> / Freckles Design</h4>
             <video className='video' src={News} autoPlay loop muted controls={false} />
             <div className='content'>
-                <h1 className='main-heading'>Will be celebrating the<br />
-                    lauch of our new site soon.</h1>
+                <h1 className='main-heading'>We'll be celebrating the<br />
+                    launch of our new site soon.</h1>
                 <div className='buttons'>
                     <button className='btn btn-outline-light btn-lg' onClick={() => window.open('https://linktr.ee/carla_antunes?fbclid=PAAaZvEwpQjeWTT9XHSHy9EZIPtEvInpc4yJlwmpryAdYnOOZ7vpgdlpG2MAA', '_blank', 'noopener noreferrer')}>Follow me</button>
                     <button className='btn btn-outline-light btn-lg' onClick={() => window.open('https://instagram.com/_freckles_design?igshid=YmMyMTA2M2Y=', '_blank', 'noopener noreferrer')}>
