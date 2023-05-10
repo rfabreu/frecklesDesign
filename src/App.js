b37818cc8a40035f750e21eac618fc1f34c5c95a
rfabreu <raabreugomes@gmail.com>
2023-05-10 01:44:14 -0400

    New gallery project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import LandingPage from './components/LandingPage';
+// import LandingPage from './components/LandingPage';
+// import ParallaxCarousel from './components/RotatingCarousel';
+import Follow from './components/Follow';
 import Footer from './components/Footer';
 import './App.css';
 
 function App() {
   return (
     <main>
-      <LandingPage />
+      {/* <LandingPage /> */}
+      {/* <ParallaxCarousel /> */}
+      <Follow />
       <Footer />
     </main>
   );
--- src/components/Follow.js
+++ src/components/Follow.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import News from "../coming_soon.mp4";
-// import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-function LandingPage() {
+function Follow() {
     return (
         <div className='App'>
-            <h4 className='header'><strong>CARLA ANTUNES</strong> / Freckles Design</h4>
-            <video className='video' src={News} autoPlay loop muted playsInline controls={false} />
             <div className='content'>
-                <h1 className='main-heading'>We'll be celebrating the<br />
-                    launch of our new site soon.</h1>
-                {/* <div className='buttons'>
+                <div className='buttons'>
                     <button className='btn btn-outline-light btn-lg' onClick={() => window.open('https://linktr.ee/carla_antunes?fbclid=PAAaZvEwpQjeWTT9XHSHy9EZIPtEvInpc4yJlwmpryAdYnOOZ7vpgdlpG2MAA', '_blank', 'noopener noreferrer')}>Follow me</button>
                     <button className='btn btn-outline-light btn-lg' onClick={() => window.open('https://instagram.com/_freckles_design?igshid=YmMyMTA2M2Y=', '_blank', 'noopener noreferrer')}>
                         <FontAwesomeIcon icon={faArrowRight} className='btn-icon' /> @_freckles_design
@@ -20,10 +15,10 @@ function LandingPage() {
                     <button className='btn btn-outline-light btn-lg' onClick={() => window.location.href = 'mailto:carla@frecklesdesign.ca'}>
                         <FontAwesomeIcon icon={faArrowRight} className='btn-icon' /> carla@frecklesdesign.ca
                     </button>
-                </div> */}
+                </div>
             </div>
         </div>
     );
 }
 
-export default LandingPage;
+export default Follow;
