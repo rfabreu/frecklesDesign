be67ae951938dbd2f8dca3fb79e63d665324de91
rfabreu <raabreugomes@gmail.com>
2023-05-14 09:41:32 -0400

    Created Follow component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 // import LandingPage from './components/LandingPage';
 import Header from './components/Header';
 import Gallery from './components/Gallery';
+import Follow from './components/Follow';
 import Footer from './components/Footer';
 import './App.css';
 
@@ -10,6 +11,7 @@ function App() {
     <main>
       <Header />
       <Gallery />
+      <Follow />
       {/* <LandingPage /> */}
       <Footer />
     </main>
